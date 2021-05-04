10df1d260e6229c002c38668df3c27fa43df4d78
vdr_rohan.singh <vdr_rohan.singh@airasia.com>
2021-05-05 00:19:22 +0530

    update local module reference

--- filestore/values/main.tf
+++ filestore/values/main.tf
@@ -8,20 +8,7 @@ locals {
   suffix = format("%s-%s", "tf", random_string.launch_id.result)
 }
 
-module "man_nas" {
-  source               = "../modules"
-  suffix               = local.suffix
-  gcp_project_id       = "searce-academy"
-  filestore_name       = "filestore"
-  filestore_tier       = "STANDARD"
-  filestore_share_name = "Documents"
-  region               = "us-west1"
-  zone                 = "b"
-  vpc_network_name     = "main-vpc"
-  network_modes        = ["MODE_IPV4"]
-}
-
-module "default" {
+module "sauv" {
   source               = "../modules"
   suffix               = local.suffix
   gcp_project_id       = "searce-academy"
